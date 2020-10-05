b1cbbb49886a79cd2014d6b8bf682bfd0896cb37
bal3000 <bal3000286@googlemail.com>
2020-10-05 17:07:11 +0100

    deck testing finished

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"strings"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	cards := newDeck()
 	if len(cards) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(cards))
 	}
+
+	if strings.TrimSpace(cards[0]) != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", cards[0])
+	}
+
+	lastCard := strings.TrimSpace(cards[len(cards)-1])
+	if lastCard != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", lastCard)
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting.txt"
+	os.Remove(fileName)
+
+	deck := newDeck()
+	deck.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+	os.Remove(fileName)
 }
